f9f63333877c7163301a8790b7e8bbf1ee4d29d0
PanharithSor16 <panharithsor16@gmail.com>
2025-05-28 16:03:36 +0700

    update the function controller category

--- src/main/java/com/e_com/rice/controller/CategoryController.java
+++ src/main/java/com/e_com/rice/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.e_com.rice.controller;
 import com.e_com.rice.models.Category;
 import com.e_com.rice.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,25 +18,38 @@ import java.util.stream.Collectors;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private Category category;
 
     @GetMapping ("/get-category")
-    public List<Category> getCategory(){
+    public ResponseEntity<List<Category>> getCategory(){
 
         List<Category> CategoryList = categoryService.getCategories();
-        return CategoryList;
+        return ResponseEntity.ok(CategoryList);
     }
     @PostMapping("/create-category")
-    public String createCategory(@RequestBody Category category){
-        Category create = new Category();
-        create.setCategory(category.getCategory());
-        create.setId(category.getId());
-        return "Success";
+    public ResponseEntity<String> createCategory(@RequestBody Category category){
+
+        category.setCategory(category.getCategory());
+        category.setId(category.getId());
+        return ResponseEntity.ok( "Success");
+    }
+    @GetMapping("category/{id}")
+    public ResponseEntity<?> getCategoryById(@PathVariable long id)  {
+        Optional<Category> categories = categoryService.getCategories().stream()
+                .filter(e ->Objects.equals(e.getId(), id)).findFirst();
+        if (categories.isPresent()){
+            return ResponseEntity.ok(categories.get());
+        }
+     return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category with ID "+ id + " Not Found");
     }
     @PutMapping("/update-category/{id}")
-    public String updateCategory(@PathVariable Long id, @RequestBody Category category){
+    public ResponseEntity<String> updateCategory(@PathVariable Long id, @RequestBody Category category1){
         Optional<Category> categories = categoryService.getCategories().stream().filter(e -> Objects.equals(e.getId(), id)).findFirst();
         if (categories.isPresent()){
+            Category exisingCategory = categories.get();
 
         }
+        return ResponseEntity.ok( "Update Success");
     }
 }
